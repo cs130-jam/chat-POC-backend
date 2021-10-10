d6b84642dd0f9fb33e600266a6cc1a3a08e7e2e2
Andrew Cummings <acummings@egnyte.com>
2021-10-10 16:20:23 -0700

    make test work

--- src/main/java/com/example/chatconcept/ChatConceptApplication.java
+++ src/main/java/com/example/chatconcept/ChatConceptApplication.java
@@ -1,6 +1,7 @@
 package com.example.chatconcept;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -8,8 +9,7 @@ import org.springframework.context.annotation.Import;
 @Import({
         ChatConceptContext.class
 })
-
-@Configuration
+@SpringBootConfiguration
 @EnableAutoConfiguration
 public class ChatConceptApplication {
 
